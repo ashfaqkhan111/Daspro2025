ea3a521a6db6c073a266149fcdfe640fac1614b4
Ashfaq Khan <ahfaqkhan97@gmail.com>
2025-10-11 21:44:18 +0700

    Test Modification 1

---
+++ jobsheet5/src/jobsheet5/ifCetakKRSNoPresensi.java
@@ -0,0 +1,30 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRSNoPresensi {
+
+	public ifCetakKRSNoPresensi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Ukt is Paid ? (true / false) :");
+		boolean UKTlunas = input.nextBoolean();
+
+		if (UKTlunas) {
+			
+			System.out.println("KRS can be printed");
+			
+		}
+		
+		else {
+			System.out.print("Pay UKT first");
+		}
+	}
+
+}
