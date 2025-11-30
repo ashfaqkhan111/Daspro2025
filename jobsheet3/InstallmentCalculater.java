35a71f754a13b9ffeb2f5a51e124ad23287fd40e
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2025-11-30 18:25:06 +0700

    Installment Calculater

--- jobsheet3/InstallmentCalculater.java
+++ jobsheet3/InstallmentCalculater.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class InstallmentCalculater {
 
 	public InstallmentCalculater() {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		Scanner input = new Scanner (System.in);
 		
