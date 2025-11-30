d32ea016c9636dcc190e9c02211966f308640fa2
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2025-11-30 23:02:35 +0700

    experiment 2.2

--- Meeting14/Coffee25410702062.java
+++ Meeting14/Coffee25410702062.java
@@ -2,7 +2,22 @@ package Meeting14;
 import java.util.*;
 
 public class Coffee25410702062 {
-public static void menu(){
+public static void menu(String custamorname, boolean ismember, String DiscountCode){
+    System.out.println("welcome "+custamorname);
+if(ismember){
+System.out.println("you are a member you get 20% discount");
+}
+
+if(DiscountCode.equals("DISKON50")){
+    System.out.println("you get 50% discount");
+}else if(DiscountCode.equals("DISKON30")){
+System.out.println("you get 30% Discount");
+
+}else{
+    System.out.println("Invalid Discount Code");
+}
+
+
 
 System.out.println("=====Coffee Shop=====");
 System.out.println("1. Balck Coffee - 15k");
@@ -10,15 +25,29 @@ System.out.println("Latte - 20k");
 System.out.println("Cappucino - 30k");
 System.out.println("Teh Tarik - 12k");
 System.out.println("Please Choose your Drink");
-
-
 }
 
 public static void main(String[] args) {
-    menu();
+    
 
+    Scanner in = new Scanner (System.in);
+
+System.out.println("enter your name :");
+String custamorname = in.nextLine();
+
+System.out.println("are you member? true/false : ");
+boolean ismember = in.nextBoolean();
+
+in.nextLine();
+
+System.out.println("Enter Discount code : ");
+String DiscountCode = in.nextLine().toUpperCase();
+
+
+
+menu(custamorname,ismember,DiscountCode);
 }
-}    
+}   
 
 
 
