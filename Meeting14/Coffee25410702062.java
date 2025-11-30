8ce28ce2aa7ad9752f28a2ee4bfd159fe218aeb5
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2025-11-30 19:57:07 +0700

    Drink menu

---
+++ Meeting14/Coffee25410702062.java
@@ -0,0 +1,24 @@
+package Meeting14;
+import java.util.*;
+
+public class Coffee25410702062 {
+public static void menu(){
+
+System.out.println("=====Coffee Shop=====");
+System.out.println("1. Balck Coffee - 15k");
+System.out.println("Latte - 20k");
+System.out.println("Cappucino - 30k");
+System.out.println("Teh Tarik - 12k");
+System.out.println("Please Choose your Drink");
+
+
+}
+
+public static void main(String[] args) {
+    menu();
+
+}
+}    
+
+
+
