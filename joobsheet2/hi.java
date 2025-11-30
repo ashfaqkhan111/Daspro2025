098c698ae0989af9fe5afc5fddd6665f37a0300a
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2025-11-30 10:33:01 +0700

    Initial commit

---
+++ joobsheet2/hi.java
@@ -0,0 +1,13 @@
+package joobsheet2;
+import java.util.*;
+
+public class hi {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello111");
+
+        int a = in.nextInt();
+        System.out.println("the:"+a);
+    }
+}
