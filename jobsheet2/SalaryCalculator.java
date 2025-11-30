f0c0640be4c6dd5771e54b0976faad0f88239a2f
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2025-11-30 18:23:40 +0700

    Salary Calculator

--- jobsheet2/SalaryCalculator.java
+++ jobsheet2/SalaryCalculator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SalaryCalculator {
 
 	public SalaryCalculator() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
