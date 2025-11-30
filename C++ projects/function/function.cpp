eaaab64ab7208802e3c72ece200d719cfe2eed7e
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2025-11-30 20:30:23 +0700

    Coffee menu

---
+++ C++ projects/function/function.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+void menu(){
+cout <<"======menu======"<<endl;
+cout <<"1. latte - 12k"<<endl;
+cout <<"teh tarik - 10K"<<endl;
+cout <<"mocha - 20k"<<endl;
+cout<<"choose your drink"<<endl;
+
+}
+
+int main(){
+
+    menu();
+    return 0;
+}
+
+
