4f06bc6713b6e3106052a9ad5ff8755b6ccb8536
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2025-12-02 10:37:48 +0700

    Gratitude_254107020262

---
+++ Meeting14/Gratitude_254107020262.java
@@ -0,0 +1,26 @@
+package Meeting14;
+
+public class Gratitude_254107020262 {
+    
+public static void main(String [] args){
+
+SayThankYou();
+
+String experission = "Thank You .... wish you all the best.";
+
+AdditionalGreeting(experission);
+
+}
+
+static void SayThankYou(){
+
+    System.out.print("thank for being the best teacher \n"+"you inspired me ");
+}
+
+static void AdditionalGreeting(String greetings){
+
+System.out.print(greetings);
+
+}
+
+}
